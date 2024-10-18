9dc37c8c033b72a145c6fd692bf416bb3964c0a1
itsjohnuser <johnarumulla@gmail.com>
2024-10-18 19:51:14 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Header from "./Header"
+import Footer from "./Footer";
+import Food from "./Food";
+import Card from "./Card";
+import Button from "./Button";
+function App() {
+  
+  return(
+    <>
+    <Header/>
+    <Food/>
+    <Footer/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Card/>
+    <Button/>
+
+    </>
+    
+  );
+}
+
+export default App
---
+++ src/Card.jsx
@@ -0,0 +1,16 @@
+
+
+function Card(){
+  return(
+    <div className="card">
+      <img className="card-image" src="https://images.pexels.com/photos/376464/pexels-photo-376464.jpeg?auto=compress&cs=tinysrgb&w=120&h=750&dpr=2" alt="profile-picture" />
+      <h2 className="card-title">John Code</h2>
+      <p className="card-para">I Learn React course</p>
+
+
+    </div>
+  )
+
+}
+
+export default Card
